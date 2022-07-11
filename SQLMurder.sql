4dc21df91d1cf46ef5ad5dd440df70f09ce2b619
Zsolt Kovac <104670003+Kovaczso@users.noreply.github.com>
2022-07-11 02:22:30 +0200

    Update SQLMurder.sql

--- SQLMurder.sql
+++ SQLMurder.sql
@@ -153,3 +153,5 @@ FROM
 --Congrats, you found the brains behind the murder! 
 --Everyone in SQL City hails you as the greatest SQL detective of all time. 
 --Time to break out the champagne!
+
+🎉🥳🎈🎊✔
