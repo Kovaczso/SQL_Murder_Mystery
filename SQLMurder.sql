26a8c2e2c7ef48e0ed16ea1bd38fde81092af168
Zsolt Kovac <104670003+Kovaczso@users.noreply.github.com>
2022-08-08 19:11:48 +0200

    Update SQLMurder.sql

--- SQLMurder.sql
+++ SQLMurder.sql
@@ -4,6 +4,7 @@ FULL GAME : https://mystery.knightlab.com/
 --The detective gave you the crime scene report, but you somehow lost it.
 --You vaguely remember that the crime was a  murder  that occurred sometime on  Jan.15, 2018  and that it took place in  SQL City .
 --Start by retrieving the corresponding crime scene report from the police department’s database.
+
 Select
   *
 FROM
@@ -12,31 +13,45 @@ WHERE
   date = 20180115
   AND type = 'murder'
   AND city = 'SQL City';
-
+  
 --Security footage shows that there were 2 witnesses.
 --The first witness lives at the last house on "Northwestern Dr".
 --The second witness, named Annabel, lives somewhere on "Franklin Ave".
-Select
-  *
-FROM
-  person
-Where
-  address_street_name = 'Northwestern Dr'
-ORDER BY
-  address_number DESC
-LIMIT
-  1;
 
+with
+  witness_1 AS(
+    Select
+      *
+    FROM
+      person
+    Where
+      address_street_name = 'Northwestern Dr'
+    ORDER BY
+      address_number DESC
+    LIMIT
+      1
+  ), witness_2 AS(
+    Select
+      *
+    FROM
+      person
+    Where
+      name LIKE '%Annabel%'
+      AND address_street_name = 'Franklin Ave'
+  )
 Select
   *
 FROM
-  person
-Where
-  name LIKE '%Annabel%'
-  AND address_street_name = 'Franklin Ave';
+  witness_1
+UNION
+Select
+  *
+FROM
+  witness_2;
 
 --1st Witness  14887 Morty Schapiro 118009 4919 Northwestern Dr 111564949
 --2nd Witness  16371 Annabel Miller 490173 490173 Franklin Ave 318771143
+
 Select
   transcript,
   person.name
@@ -80,6 +95,7 @@ WHERE
 --Only gold members have those bags. The man got into a car with a plate that included "H42W".
 --Annabel Miller
 --I saw the murder happen, and I recognized the killer from my gym when I was working out last week on January the 9th.
+
 Select
   get_fit_now_member.person_id,
   person.name
@@ -95,6 +111,7 @@ Where
   AND drivers_license.plate_number LIKE '%H42W%';
 
 -- 67318 Jeremy Bowers
+
 Select
   *
 FROM
@@ -123,9 +140,11 @@ FROM
   interview
 Where
   person_id = 67318 
+  
   -- I was hired by a woman with a lot of money.
   --I don't know her name but I know she's around 5'5" (65") or 5'7" (67").
   --She has red hair and she drives a Tesla Model S. I know that she attended the SQL Symphony Concert 3 times in December 2017.
+  
 Select
   drivers_license.id,
   person.name,
@@ -140,7 +159,10 @@ WHERE
   AND drivers_license.car_make = 'Tesla'
   AND drivers_license.car_model = 'Model S'
   AND facebook_event_checkin.event_name = 'SQL Symphony Concert'
-  AND facebook_event_checkin.date LIKE '%201712%';
+  AND facebook_event_checkin.date LIKE '%201712%'
+  LIMIT 1;
+  
+ -- 202298 Miranda Priestly 310000
 
 INSERT INTO
   solution
